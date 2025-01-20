89dcf96459d8d98e0ff3c77a8307d41f79c2ebe1
Keshav Mehta <Keshav.Mehta@student.csulb.edu>
2025-01-20 02:01:35 -0800

    Fix: Use process.env.PORT instead of HTTP_PORT

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use('/contact', contactRoute)
 
 
 
-const port = process.env.HTTP_PORT || 3000 
+const port = process.env.PORT || 3000 
 app.listen(port, () =>{
     console.log(`Server Running 😁 on port ${port} `)
 })
