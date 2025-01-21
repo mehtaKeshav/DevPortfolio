9e26c377e5a549ffb305c5d915bec99d95bdeb58
Keshav Mehta <Keshav.Mehta@student.csulb.edu>
2025-01-20 16:24:37 -0800

    Test: generate-config.js

--- frontend/generate-config.js
+++ frontend/generate-config.js
@@ -1,14 +1,14 @@
 const fs = require("fs");
 
-// Load environment variables from .env file
-require("dotenv").config();
+// NOTE: for local environment load environment variables from .env file
+require("dotenv").config({ path: "./frontend/.env" });
 
 // Replace placeholders in config.template.js
 const template = fs.readFileSync("./frontend/config.template.js", "utf8");
 
+console.log("Loaded API_BASE_URL:", process.env.API_BASE_URL)
 const config = template
   .replace("REPLACE_API_BASE_URL", process.env.API_BASE_URL)
-
 // Write the final config.js to the frontend directory
 fs.writeFileSync("./frontend/config.js", config);
 
